10c664b0d2d0df333f319b1ff9ce863f3ef5f362
Ronald <Ronald@WINDOWS10>
2016-12-18 22:33:48 -0500

    cambios a postgres

--- app/Config/database.php
+++ app/Config/database.php
@@ -80,4 +80,24 @@ class DATABASE_CONFIG {
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
+	
+	public function __construct() {
+		if (getenv("OPENSHIFT_MYSQL_DB_HOST")){
+		$this->default['host']       = getenv("OPENSHIFT_MYSQL_DB_HOST");
+		$this->default['port']       = getenv("OPENSHIFT_MYSQL_DB_PORT");
+		$this->default['login']      = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+		$this->default['password']   = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+		$this->default['database']   = getenv("OPENSHIFT_APP_NAME");
+		$this->default['datasource'] = 'Database/Mysql';
+		$this->test['datasource']    = 'Database/Mysql';
+		}else{
+		$this->default['host']       = getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
+		$this->default['port']       = getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
+		$this->default['login']      = getenv("OPENSHIFT_POSTGRESQL_DB_USERNAME");
+		$this->default['password']   = getenv("OPENSHIFT_POSTGRESQL_DB_PASSWORD");
+		$this->default['database']   = getenv("OPENSHIFT_APP_NAME");
+		$this->default['datasource'] = 'Database/Postgres';
+		$this->test['datasource']    = 'Database/Postgres';
+		}
+	}
 }
