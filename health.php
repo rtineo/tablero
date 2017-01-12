c69ed7214c8172e44aafe9c1357e001b0f532dac
rtineo <dinho.rhts@gmail.com>
2017-01-12 14:43:39 -0500

    Update health.php
    se ha comentado la conexion a mysql

--- health.php
+++ health.php
@@ -5,12 +5,12 @@ $username = getenv("DATABASE_USER");
 $password = getenv("DATABASE_PASSWORD");
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+//$conn = new mysqli($servername, $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
-    header("HTTP/1.1 503 Service Unavailable");
-    die("Connection failed: " . $conn->connect_error);
-}
+//if ($conn->connect_error) {
+//    header("HTTP/1.1 503 Service Unavailable");
+//    die("Connection failed: " . $conn->connect_error);
+//}
 echo "OK";
 ?>
